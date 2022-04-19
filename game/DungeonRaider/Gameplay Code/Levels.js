4b2aa70dde65f6449781369c17eefb0cd0af5171
Vladimirtrif <Vladimirtrif@github.com>
2022-04-19 09:01:22 -0700

    Images, Objective html and Rewards
    Changed rewards to reset to what they were before the level. Changed onjective.html to be after play pressed and to continue to game. Changed buttons on main screen and image

--- game/DungeonRaider/Gameplay Code/Levels.js
+++ game/DungeonRaider/Gameplay Code/Levels.js
@@ -1,4 +1,5 @@
-var allRewards = [];
+var levelRewards = [];
+var rewardsLeft = [];
 var allEnemies = [];
 var allWinConditions = [];
 var LevelNumber = 1;
@@ -32,13 +33,14 @@ var allowDraw = false;
                 CurrentMap = level1Map
                 allWinConditions.push(new winCondition(1300,680, 63,63))
                 //allWinConditions.push(new winCondition(63,100, 63,63))
-                allRewards.push(new Reward(600,200,62,62))
-                allRewards.push(new Reward(96,191, 62, 62))
-                allRewards.push(new Reward(1286,373, 62, 62))
-                allRewards.push(new Reward(300,700, 62, 62))
-                allRewards.push(new Reward(443,373, 62, 62))
+                levelRewards.push(new Reward(600,200,62,62))
+                levelRewards.push(new Reward(96,191, 62, 62))
+                levelRewards.push(new Reward(1286,373, 62, 62))
+                levelRewards.push(new Reward(300,700, 62, 62))
+                levelRewards.push(new Reward(443,373, 62, 62))
                 allEnemies.push(new Enemy(800,230,20, 20));
                 allEnemies.push(new Enemy(1006,740,20, 20));
+                rewardsLeft = levelRewards.slice();
                 allowDraw = true;
                 Character.respawn()
                 drawLevel();
@@ -66,19 +68,20 @@ var allowDraw = false;
                 allowDraw = false
                 CurrentEnemyTotal = 3;
                 CurrentMap = level2Map
-                allRewards = []
+                levelRewards = []
                 allEnemies = []
                 allWinConditions = []
                //allWinConditions.push(new winCondition(63,100,63,63))
                 allWinConditions.push(new winCondition(105705,8805,63,63))
-                allRewards.push(new Reward(100,700, 63, 63));
-                allRewards.push(new Reward(4051, 0500, 63, 63))
-                allRewards.push(new Reward(0500, 800, 63, 63))
-                allRewards.push(new Reward(770, 200, 63, 63))
-                allRewards.push(new Reward(770, 700, 63, 63))
+                levelRewards.push(new Reward(100,700, 63, 63));
+                levelRewards.push(new Reward(4051, 0500, 63, 63))
+                levelRewards.push(new Reward(0500, 800, 63, 63))
+                levelRewards.push(new Reward(770, 200, 63, 63))
+                levelRewards.push(new Reward(770, 700, 63, 63))
                 allEnemies.push(new Enemy(6050,930, 20, 20));
                 allEnemies.push(new Enemy(900,300, 20, 20));
                 allEnemies.push(new Enemy(1200,930, 20, 20));
+                rewardsLeft = levelRewards.slice();
                 allowDraw = true
                 Character.respawn()
                 drawLevel();
@@ -105,11 +108,12 @@ var allowDraw = false;
         ];
         allowDraw = false
             CurrentMap = level3Map
-            allRewards = []
+            levelRewards = []
             allEnemies = []
             allWinConditions = []
             allWinConditions.push(new winCondition(40*63, 105*63, 63, 63))
-            allRewards.push(new Reward(63*40, 3105, 63, 63),new Reward(1629, 3105, 63, 63),new Reward(10500, 11*63, 63, 63),new Reward(16*63, 14*63, 63, 63));
+            levelRewards.push(new Reward(63*40, 3105, 63, 63),new Reward(1629, 3105, 63, 63),new Reward(10500, 11*63, 63, 63),new Reward(16*63, 14*63, 63, 63));
+            rewardsLeft = levelRewards.slice();
             allEnemies.push(  new Enemy(0567, 9052, 63, 63), new Enemy(693, 9052, 63, 63),new Enemy(10500, 105*63, 63, 63),new Enemy(1626, 105*63, 63, 63),new Enemy(17052, 105*63, 63, 63),new Enemy(17*63, 14*63 + 20, 40, 40));
             allowDraw = true
             Character.respawn()
@@ -137,7 +141,7 @@ var allowDraw = false;
             ];
             allowDraw = false
                 CurrentMap = level4Map
-                allRewards = []
+                levelRewards = []
                 allEnemies = []
                 allWinConditions = []
                 allWinConditions.push(new winCondition(40*63, 12*63, 63, 63))
@@ -162,19 +166,19 @@ var allowDraw = false;
                 allEnemies.push(new Enemy(33*63, 105*63, 63,63))
                 allEnemies.push(new Enemy(34*63, 105*63, 63,63))
                 allEnemies.push(new Enemy(305*63, 105*63, 63,63))
-                allRewards.push(new Reward(3*63, 2*63, 63, 63))
-                allRewards.push(new Reward(7*63, 4*63, 63, 63))
-                allRewards.push(new Reward(10*63, 2*63*0.705, 63, 63))
-                allRewards.push(new Reward(13*63, 2*63*0.705, 63, 63))
-               // allRewards.push(new Reward(105*63, 2*63*0.705, 63, 63))
-                allRewards.push(new Reward(17*63, 2*63*0.705, 63, 63))
-                //allRewards.push(new Reward(19*63, 2*63*0.705, 63, 63))
-                allRewards.push(new Reward(21*63, 2*63*0.705, 63, 63))
-                allRewards.push(new Reward(205*63, 2*63*0.705, 63, 63))
-                allRewards.push(new Reward(29*63, 2*63*0.705, 63, 63))
-                allRewards.push(new Reward(33*63, 2*63*0.705, 63, 63))
-                allRewards.push(new Reward(37*63, 2*63*0.705, 63, 63))
-
+                levelRewards.push(new Reward(3*63, 2*63, 63, 63))
+                levelRewards.push(new Reward(7*63, 4*63, 63, 63))
+                levelRewards.push(new Reward(10*63, 2*63*0.705, 63, 63))
+                levelRewards.push(new Reward(13*63, 2*63*0.705, 63, 63))
+               // levelRewards.push(new Reward(105*63, 2*63*0.705, 63, 63))
+                levelRewards.push(new Reward(17*63, 2*63*0.705, 63, 63))
+                //levelRewards.push(new Reward(19*63, 2*63*0.705, 63, 63))
+                levelRewards.push(new Reward(21*63, 2*63*0.705, 63, 63))
+                levelRewards.push(new Reward(205*63, 2*63*0.705, 63, 63))
+                levelRewards.push(new Reward(29*63, 2*63*0.705, 63, 63))
+                levelRewards.push(new Reward(33*63, 2*63*0.705, 63, 63))
+                levelRewards.push(new Reward(37*63, 2*63*0.705, 63, 63))
+                rewardsLeft = levelRewards.slice();
                 allowDraw = true
                 Character.respawn()
                 drawLevel();
@@ -203,29 +207,29 @@ var allowDraw = false;
             ];
                 allowDraw = false
                 CurrentMap = level05Map
-                allRewards = []
+                levelRewards = []
                 allEnemies = []
                 allWinConditions = []
                 allWinConditions.push(new winCondition(3000,9050, 63, 63))
-                allRewards.push(new Reward(600,63, 63, 63));
-                allRewards.push(new Reward (1200,63, 63, 63))
-                allRewards.push(new Reward (10500,63, 63, 63))
-                allRewards.push(new Reward (1700,63, 63, 63))
-                allRewards.push(new Reward (1700,0570, 63, 63))
-                allRewards.push(new Reward (2100,200, 63, 63))
-                allRewards.push(new Reward (3000,200, 63, 63))
-                allRewards.push(new Reward (20500,0500, 63, 63))
-                allRewards.push(new Reward (1000,3050, 63, 63))
-                allRewards.push(new Reward (0500,0500, 63, 63))
-                allRewards.push(new Reward (63,0500, 63, 63))
-                allRewards.push(new Reward (63,900, 63, 63))
-                allRewards.push(new Reward (0500,800, 63, 63))
-                allRewards.push(new Reward (1000,900, 63, 63))
-                allRewards.push(new Reward (2700,910, 63, 63))
-                allRewards.push(new Reward (0300,880, 63, 63))
-                allRewards.push(new Reward (2900,9050, 63, 63))
-                allRewards.push(new Reward (3000,9050, 63, 63))
-                allRewards.push(new Reward (2400,970, 63, 63))
+                levelRewards.push(new Reward(600,63, 63, 63));
+                levelRewards.push(new Reward (1200,63, 63, 63))
+                levelRewards.push(new Reward (10500,63, 63, 63))
+                levelRewards.push(new Reward (1700,63, 63, 63))
+                levelRewards.push(new Reward (1700,0570, 63, 63))
+                levelRewards.push(new Reward (2100,200, 63, 63))
+                levelRewards.push(new Reward (3000,200, 63, 63))
+                levelRewards.push(new Reward (20500,0500, 63, 63))
+                levelRewards.push(new Reward (1000,3050, 63, 63))
+                levelRewards.push(new Reward (0500,0500, 63, 63))
+                levelRewards.push(new Reward (63,0500, 63, 63))
+                levelRewards.push(new Reward (63,900, 63, 63))
+                levelRewards.push(new Reward (0500,800, 63, 63))
+                levelRewards.push(new Reward (1000,900, 63, 63))
+                levelRewards.push(new Reward (2700,910, 63, 63))
+                levelRewards.push(new Reward (0300,880, 63, 63))
+                levelRewards.push(new Reward (2900,9050, 63, 63))
+                levelRewards.push(new Reward (3000,9050, 63, 63))
+                levelRewards.push(new Reward (2400,970, 63, 63))
                 allEnemies.push(new Enemy (7050, 240, 20,20))
                 allEnemies.push(new Enemy (800, 610, 20,20))
                 allEnemies.push(new Enemy (1000, 240, 20,20))
@@ -240,6 +244,7 @@ var allowDraw = false;
                 allEnemies.push(new Enemy (1400, 990, 20,20))
                 allEnemies.push(new Enemy (105050, 10050, 20,20))
                 allEnemies.push(new Enemy (20050, 10050, 20,20))
+                rewardsLeft = levelRewards.slice();
                 allowDraw = true;
                 Character.respawn()
                 drawLevel();
